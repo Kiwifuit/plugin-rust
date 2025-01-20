e6aeb40a24609a71d144d19196c51136eab4a1e1
Kiwifuit <mahkiwi123@gmail.com>
2025-01-20 19:54:56 +0800

    feat: plugin metadata

--- tplug-common/src/lib.rs
+++ tplug-common/src/lib.rs
@@ -1,14 +1,6 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+#[cfg(feature = "plugin")]
+pub struct PluginMetadata<'a> {
+    name: &'a str,
+    version: &'a str,
+    init: fn() -> Box<dyn std::any::Any>,
 }
